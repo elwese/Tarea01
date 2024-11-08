3cb188078c8252cf6b5529bf7fdc904a33331cd4
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2024-11-08 18:50:18 -0500

    Soporte.java

--- Soporte.java
+++ Soporte.java
@@ -1,11 +1,11 @@
 public class Soporte {
     public boolean resolverIncidente(Usuario usuario) {
-        // Lógica mínima
+        
         return true;
     }
 
     public boolean derivarProblema(Administrador administrador) {
-        // Lógica mínima
+        
         return true;
     }
     
