fe0237759f3dc80fadcaca2ded904ad0ed77d705
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2024-11-08 18:56:42 -0500

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.tarea1;
+
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String usuario;
+    private String contraseña;
+
+    public boolean logIn() {
+        return true;
+    }
+
+    public boolean logOut() {
+        return true;
+    }
+
+    public boolean reportarProblema(String descripcion) {
+        return true;
+    }
+}
