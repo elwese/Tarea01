4614cb966754d913d8a9045838fe13840c37c92c
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2024-11-08 19:26:27 -0500

    Add files via upload

---
+++ Administrador.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.tarea1;
+
+
+public class Administrador {
+    public Evento crearEvento() {
+        return new Evento();
+    } 
+}
