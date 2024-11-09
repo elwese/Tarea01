a96a7f3c01fa60fe941f6cc0da2c945f37fb3ba3
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2024-11-08 19:07:29 -0500

    Add files via upload

---
+++ Devolucion.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.tarea1;
+
+public class Devolucion {
+    private Evento evento;
+    private Cobro orden;
+
+    public boolean realizarDevolucion() {
+        return true;
+    }
+
+    public boolean realizarCambioFecha(String nuevaFecha) {
+        return true;
+    }
+}
