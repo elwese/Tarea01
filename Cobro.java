a92af9043957bc9ad5daf634a1b6ee00c0f29756
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2024-11-08 19:15:08 -0500

    Add files via upload

---
+++ Cobro.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.tarea1;
+
+import java.util.List;
+
+public class Cobro {
+    private List<String> mediosDisponibles;
+
+    public boolean realizarPago() {
+        return true;
+    }
+}
