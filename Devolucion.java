71f6dd26fbaf45cf75653d852f629401a67a849c
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2024-11-08 19:07:53 -0500

    Devolucion.java

--- Devolucion.java
+++ Devolucion.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ec.edu.espol.tarea1;
 
 public class Devolucion {
@@ -15,4 +11,5 @@ public class Devolucion {
     public boolean realizarCambioFecha(String nuevaFecha) {
         return true;
     }
+    
 }
