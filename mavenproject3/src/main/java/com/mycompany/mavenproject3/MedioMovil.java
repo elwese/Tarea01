69fd9a1fbad97dd72c31abe40b5a919b94efac30
jeloarte <149507931+jeloarte@users.noreply.github.com>
2025-01-17 22:27:19 -0500

    Update MedioMovil.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/MedioMovil.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/MedioMovil.java
@@ -3,9 +3,15 @@ package com.mycompany.mavenproject3;
 
 
 public class MedioMovil implements MedioPago{
-    String numeroTelf;
+    private String numeroTelf;
+
+    public MedioMovil(String numeroTelf) {
+        this.numeroTelf = numeroTelf;
+    }
+
     @Override
-    public boolean realizarPago() {
+    public boolean realizarPago(double monto) {
+        System.out.println("Pago de $" + monto + " realizado mediante pago móvil al número: " + numeroTelf);
         return true;
     }
     
