df1b57c556a7915bcc30316ca0d52f36fc7c86d8
jeloarte <149507931+jeloarte@users.noreply.github.com>
2025-01-17 21:55:59 -0500

    Update VIPFactory.java
    faltaban parámetros necesarios para la creación del ticket, como asiento, dueño, y otros atributos necesarios.

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/VIPFactory.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/VIPFactory.java
@@ -4,7 +4,7 @@ package com.mycompany.mavenproject3;
 
 public class VIPFactory extends TicketFactory{
     @Override
-    public Ticket crearTicket(String asiento, Usuario dueño, Evento evento, String beneficios) {
-        return new TicketVIP(asiento, dueño, evento, beneficios);
+    public Ticket crearTicket(Estado estado, String asiento, Usuario dueño, Evento evento, String beneficios) {
+        return new TicketVIP(estado, asiento, dueño, evento, beneficios);
     }
 }
