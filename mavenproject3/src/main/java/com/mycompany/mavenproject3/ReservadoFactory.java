744e4eb099c0504bed7d7094b51a5a347a55e71a
jeloarte <149507931+jeloarte@users.noreply.github.com>
2025-01-17 21:50:33 -0500

    Update ReservadoFactory.java
    faltaban parámetros necesarios para la creación del ticket, como asiento, dueño, y otros atributos necesarios.

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/ReservadoFactory.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/ReservadoFactory.java
@@ -4,8 +4,8 @@ package com.mycompany.mavenproject3;
 
 public class ReservadoFactory extends TicketFactory{
     @Override
-    public Ticket crearTicket() {
-        //retorna un ticket general
-        return new TicketReservado();
+    @Override
+    public Ticket crearTicket(String asiento, Usuario dueño, Evento evento, String beneficios) {
+        return new TicketReservado(asiento, dueño, evento, beneficios);
         }
 }
