69e9310ff508cd0025d948fba5952061ef2d2154
jeloarte <149507931+jeloarte@users.noreply.github.com>
2025-01-17 22:28:39 -0500

    Update MedioTransferencia.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/MedioTransferencia.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/MedioTransferencia.java
@@ -2,9 +2,15 @@
 package com.mycompany.mavenproject3;
 
 public class MedioTransferencia implements MedioPago{
-    String numeroCuenta;
+    private String numeroCuenta;
+
+    public MedioTransferencia(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
     @Override
-    public boolean realizarPago() {
+    public boolean realizarPago(double monto) {
+        System.out.println("Pago de $" + monto + " realizado mediante transferencia bancaria: " + numeroCuenta);
         return true;
     }
     
