f0e8111a25f9bd7f3f94240a2130df44ae52f7df
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2025-01-30 17:44:26 -0500

    EstadoVendido.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/EstadoVendido.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/EstadoVendido.java
@@ -1,19 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.mavenproject3;
 
-/**
- *
- * @author Cesar
- */
 public class EstadoVendido implements Estado{
 
-    @Override
+   @Override
     public boolean cambiarEstado(String estado) {
+        System.out.println("Ticket: "+estado);
         return true;
         //cambia el estado
     }
-    
+   
 }
