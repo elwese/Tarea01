398dd8c15e1be08796a781bf9d9c5eeb60c753d6
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2025-01-30 17:50:50 -0500

    CanalSMS.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/CanalSMS.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/CanalSMS.java
@@ -1,18 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.mavenproject3;
 
-/**
- *
- * @author Cesar
- */
 public class CanalSMS extends CanalBase{
-    String numero;
+    private String numero;
     @Override
     public boolean enviarNotificacion() {
+        System.out.println("Enviando notificacion por SMS");
         return true;
         //envia por sms
     }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
 }
