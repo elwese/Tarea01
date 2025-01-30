c4fb42fce391b2994dd47baa1609b8be613a092d
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2025-01-30 17:45:14 -0500

    EstadoReservado.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/EstadoReservado.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/EstadoReservado.java
@@ -1,11 +1,10 @@
-
 package com.mycompany.mavenproject3;
-
 
 public class EstadoReservado implements Estado{
 
     @Override
     public boolean cambiarEstado(String estado) {
+        System.out.println("Ticket: "+estado);
         return true;
         //cambia el estado
     }
