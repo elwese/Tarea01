784c0793af88b389226d1cf5ae30a8b62b722023
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2025-01-30 17:46:17 -0500

    EstadoDisponible.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/EstadoDisponible.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/EstadoDisponible.java
@@ -1,17 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.mavenproject3;
 
-/**
- *
- * @author Cesar
- */
 public class EstadoDisponible implements Estado{
 
-    @Override
+    @@Override
     public boolean cambiarEstado(String estado) {
+        System.out.println("Ticket: "+estado);
         return true;
         //cambia el estado
     }
