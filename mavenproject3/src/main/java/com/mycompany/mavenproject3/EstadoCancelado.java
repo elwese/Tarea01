478e707850afc8626aa774ca0452f695de866393
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2025-01-30 17:47:08 -0500

    EstadoCancelado.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/EstadoCancelado.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/EstadoCancelado.java
@@ -1,17 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.mavenproject3;
 
-/**
- *
- * @author Cesar
- */
 public class EstadoCancelado implements Estado{
+    
     @Override
     public boolean cambiarEstado(String estado) {
+        System.out.println("Ticket Cancelado: "+estado);
         return true;
         //cambia el estado
     }
+    
 }
