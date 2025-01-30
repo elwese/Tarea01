d2f452292f00ee3311379db20be11638a27841a6
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2025-01-30 17:33:04 -0500

    Notificador.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/Notificador.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/Notificador.java
@@ -1,11 +1,10 @@
-
 package com.mycompany.mavenproject3;
-
 
 public class Notificador implements Suscriptor{
 
     @Override
     public boolean enviarNotificacion() {
+        System.out.println("Enviando notificación....");
         return true;
     }
     
