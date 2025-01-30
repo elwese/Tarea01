916fb8567af1dc92d0222fd8aab65a5c78f234c2
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2025-01-30 16:36:06 -0500

    Usuario.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/Usuario.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/Usuario.java
@@ -2,10 +2,21 @@
 package com.mycompany.mavenproject3;
 
 public class Usuario {
-    String nombre;
-    String apellido;
-    String usuario;
-    String contraseña;
+    private String nombre;
+    private String apellido;
+    private String usuario;
+    private String contraseña;
+
+    public Usuario(String nombre, String apellido, String usuario, String contraseña) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    public Usuario() {
+    }
+    
     public boolean logIn(){
         return true;
     }
@@ -16,4 +27,37 @@ public class Usuario {
     public boolean reportarProblema(String descripcion){
         return true;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
 }
