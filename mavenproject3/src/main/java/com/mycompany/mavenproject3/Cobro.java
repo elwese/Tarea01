98b54cd6ba77294330638760a098c8e1fe2e0162
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2025-01-30 17:48:47 -0500

    Cobro.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/Cobro.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/Cobro.java
@@ -1,15 +1,27 @@
-
 package com.mycompany.mavenproject3;
 
-
 public class Cobro {
-    MedioPago medio;
-    Suscriptor[] suscriptores;
-    public MedioPago crearPago(MedioPago medio){
+    private MedioPago medio;
+    private Suscriptor[] suscriptores;
+    
+    public boolean crearPago(MedioPago medio){
+        this.medio = medio;
+        return true;
+    }
+
+     public boolean procesarPago(double monto) {
+        if (monto <= 0) {
+            throw new IllegalArgumentException("El monto debe ser positivo");
+        }
+        return medio.realizarPago(monto);
+    }
+
+    public MedioPago getMedio() {
+        return medio;
+    }
+
+    public void setMedio(MedioPago medio) {
         this.medio = medio;
     }
 
-     public ProcesarPago(double monto){
-        medio.realizarPago(monto);
-    }
 }
