a1cb081b367cf546415a34d0cb2235197663b8c6
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2025-01-30 17:52:31 -0500

    CanalBase.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/CanalBase.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/CanalBase.java
@@ -1,10 +1,20 @@
-
 package com.mycompany.mavenproject3;
 
-public class CanalBase extends Notificador{
-    Notificador base;
+public class CanalCorreo extends CanalBase{
+    private String direccion;
+    
     @Override
     public boolean enviarNotificacion() {
+        System.out.println("Enviando notificacion por Correo.....");
         return true;
     }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
 }
