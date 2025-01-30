36c40e99887443a3999a24f23f3880f4638daa2b
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2025-01-30 17:51:42 -0500

    CanalMovil.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/CanalMovil.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/CanalMovil.java
@@ -1,12 +1,21 @@
-
 package com.mycompany.mavenproject3;
 
-
 public class CanalMovil extends CanalBase{
-    String ususarioMobil;
+    private String ususarioMobil;
+    
     @Override
     public boolean enviarNotificacion() {
+        System.out.println("Enviando notificación por el móvil....");
         return true;
         //envia por canal mobil
     }
+
+    public String getUsusarioMobil() {
+        return ususarioMobil;
+    }
+
+    public void setUsusarioMobil(String ususarioMobil) {
+        this.ususarioMobil = ususarioMobil;
+    }
+    
 }
